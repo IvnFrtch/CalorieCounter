a91bc7255f96a272f58e2d4142faca6bf14230cd
shadycodee <mikojohn158@gmail.com>
2024-11-12 13:09:49 +0800

    utilized login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
     };
 
     return (
-      <div style={{ display: 'flex', height: '100vh', width: '100vw' }}>
+      <div style={{ display: 'flex', height: '97vh', width: '100%' }}>
         <CustomSnackbar
           open={snackbarOpen}
           onClose={handleCloseSnackbar}
@@ -65,8 +65,8 @@ const Login = () => {
             position: 'absolute',
             top: 0,
             left: 0,
-            width: '100vw',
-            height: '100vh',
+            width: '100%',
+            height: '100%',
             objectFit: 'cover',
             zIndex: -1
           }} 
